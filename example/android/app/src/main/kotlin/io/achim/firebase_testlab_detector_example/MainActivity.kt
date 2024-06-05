10a135903684b3b2d0a2ce1e3b35767728540d9f
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2024-06-05 12:57:21 +0100

    flutter create . to update Android files

--- example/android/app/src/main/kotlin/io/achim/firebase_testlab_detector_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/io/achim/firebase_testlab_detector_example/MainActivity.kt
@@ -2,5 +2,4 @@ package io.achim.firebase_testlab_detector_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
