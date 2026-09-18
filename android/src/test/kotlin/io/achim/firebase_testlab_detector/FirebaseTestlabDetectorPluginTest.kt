cd78f0a75edbf21c00d0cd4e78f66f54446b7104
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2026-09-18 07:34:59 +0400

    Complete the built-in Kotlin migration

--- android/src/test/kotlin/io/achim/firebase_testlab_detector/FirebaseTestlabDetectorPluginTest.kt
+++ android/src/test/kotlin/io/achim/firebase_testlab_detector/FirebaseTestlabDetectorPluginTest.kt
@@ -7,7 +7,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import kotlin.test.Test
+import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 
 /*
