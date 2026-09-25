e88d3bbc8fb1535cd57a48ea7854feae2a7aa478
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2026-09-25 12:15:30 +0400

    Configure Java before Android checks

--- tool/check.sh
+++ tool/check.sh
@@ -10,6 +10,13 @@ fi
 repository_root="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
 android_device_id="$1"
 
+if [[ -z "${JAVA_HOME:-}" ]]; then
+  flutter_configuration="$(flutter config --machine)"
+  if [[ "$flutter_configuration" =~ \"jdk-dir\"[[:space:]]*:[[:space:]]*\"([^\"]+)\" ]]; then
+    export JAVA_HOME="${BASH_REMATCH[1]}"
+  fi
+fi
+
 cd "$repository_root"
 flutter pub get --no-example
 dart format --page-width 120 --output=none --set-exit-if-changed lib test example/lib example/test example/integration_test
@@ -21,13 +28,6 @@ flutter pub get --enforce-lockfile --no-example
 dart analyze
 flutter test --no-pub test
 flutter test --no-pub integration_test -d "$android_device_id"
-
-if [[ -z "${JAVA_HOME:-}" ]]; then
-  flutter_configuration="$(flutter config --machine)"
-  if [[ "$flutter_configuration" =~ \"jdk-dir\"[[:space:]]*:[[:space:]]*\"([^\"]+)\" ]]; then
-    export JAVA_HOME="${BASH_REMATCH[1]}"
-  fi
-fi
 
 "$repository_root/android/gradlew" -p "$repository_root/example/android" :firebase_testlab_detector:testDebugUnitTest --no-daemon
 
